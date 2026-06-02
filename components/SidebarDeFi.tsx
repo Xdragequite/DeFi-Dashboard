dc341953ab366e019acf28eb00ac0910f2557440
Xdragequite <barkunovs390@gmail.com>
2026-06-03 01:59:14 +0300

    well I did not made much , gotta finish SidebarDeFi tomorrow

--- components/SidebarDeFi.tsx
+++ components/SidebarDeFi.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+'use client'
+import React,{useState} from 'react'
+import { Menu, X, House, Settings, User } from 'lucide-react';
 
 function SidebarDeFi() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSidebar = () => setIsOpen((prev)=>!prev);
   return (
-    <div>Sidebar</div>
+    <>
+    {/* Оверлей для мобилок */}
+    {isOpen && (
+      <div className="fixed inset-0 z-40 bg-black/50 transition-opacity lg:hidden" onClick={toggleSidebar}></div>
+    )}
+    {/* Боковая панель */}
+    <aside className=''>
+      
+    </aside>
+    </>
   )
 }
 
