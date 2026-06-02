7e06a4e5fb28432985188a957251c57fa54fe210
Xdragequite <barkunovs390@gmail.com>
2026-06-02 23:27:53 +0300

    cleared all to get started

---
+++ components/ButtonDeFi.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ButtonDeFi() {
+  return (
+    <div>ButtonDeFi</div>
+  )
+}
+
+export default ButtonDeFi
---
+++ components/SidebarDeFi.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SidebarDeFi() {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default SidebarDeFi
